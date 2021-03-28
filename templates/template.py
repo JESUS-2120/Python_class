394fc7a951ac4404b96ef64c64bcbeb3008a91fb
chucho2120 <victorec@lcg.unam.mx>
2021-03-27 20:56:24 -0600

    Entregando el template

---
+++ templates/template.py
@@ -0,0 +1,24 @@
+'''NAME
+        Template generico
+
+VERSION
+        1.0
+
+AUTHOR
+        Victor Jesus Enriquez Castro
+
+DESCRIPTION
+        Inserte descripcion del programa
+
+CATEGORY
+        Inserte categoria ej. Genomic Sequence
+
+USAGE   
+        ej.
+        % python AT-percentage.py -i filename
+
+        example
+
+        % python AT-percentage.py -i sequence.txt
+
+'''
